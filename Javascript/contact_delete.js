30373bf4d688e277e58f51cb7e43f198ca124ed7
Gabe Khofri <izotope115@gmail.com>
2021-01-04 17:05:27 -0800

    added working ssjs delete

--- Javascript/contact_delete.js
+++ Javascript/contact_delete.js
@@ -1,9 +1,9 @@
 <script runat=server>
     Platform.Load("Core", "1");
 
-    var url = 'https://{AUTH BASE URI}.auth.marketingcloudapis.com/v2/token';
+    var url = 'https://{{AUTH_BASE_URI}}.auth.marketingcloudapis.com/v2/token';
     var contentType = 'application/json';
-    var payload = '{"grant_type": "client_credentials","client_id": "{CLIENT ID}","client_secret": "{CLIENT SECRET}","account_id":"{MID}"}';
+    var payload = '{"grant_type": "client_credentials","client_id": "{{CLIENT_ID}}","client_secret": "{{CLIENT_SECRET}}","account_id":"{{MID}}"}';
 
     var accessTokenResult = HTTP.Post(url, contentType, payload);
     var accessToken = Platform.Function.ParseJSON(accessTokenResult["Response"][0]).access_token;
@@ -12,8 +12,8 @@ if(accessToken !='')
 
 {
 try {
-var deleteUrl = 'https://{REST BASE URI}.rest.marketingcloudapis.com/contacts/v1/contacts/actions/delete?type=listReference';
-var payload1 = '{"deleteOperationType": "ContactAndAttributes","targetList": {"listType": {"listTypeID": 3},"listKey": "{DE KEY}"},"deleteListWhenCompleted": false,"deleteListContentsWhenCompleted": false}';
+var deleteUrl = 'https://{{REST_BASE_URI}}.rest.marketingcloudapis.com/contacts/v1/contacts/actions/delete?type=listReference';
+var payload1 = '{"deleteOperationType": "ContactAndAttributes","targetList": {"listType": {"listTypeID": 3},"listKey": "{{DE KEY}}"},"deleteListWhenCompleted": false,"deleteListContentsWhenCompleted": false}';
 
 var headerNames = ["Authorization"];
 var s1="Bearer ";
